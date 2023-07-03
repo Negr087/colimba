6114fa20482537c40645487109eb95cf8cd2cfb4
Seba Ibañez <130003729+Negr087@users.noreply.github.com>
2023-07-03 13:15:15 -0300

    1.12 arreglado

--- 1/12/1.12/main.c
+++ 1/12/1.12/main.c
@@ -9,14 +9,21 @@ int main()
     int state = OUT;
 
     while ((c = getchar()) != EOF) {
-        if (c == ' ' || c == '\n' || c == '\t'){
+        if (state == IN) {
+        if (c == ' ' || c == '\n' || c == '\t') {
             state = OUT;
-            c = '\n';
+            printf("\n");
         }
+        else {
+            putchar (c); }
+    }
         else if (state == OUT) {
-                state = IN;
+            if (c == ' ' || c == '\n' || c == '\t') {
+
+            } 
+            else if (state == OUT) {
+            state = IN;
+            putchar (c); }
+        }
+return 0;
     }
-        putchar (c);
-    }
-    return 0;
-}
