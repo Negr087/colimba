c9fd2c0ca8feab332a5d47dae466e90bb501b0e8
Seba Ibañez <130003729+Negr087@users.noreply.github.com>
2023-07-03 19:51:44 -0300

    1.16 arreglado

--- 1/16/1.16/main.c
+++ 1/16/1.16/main.c
@@ -1,56 +1,52 @@
 #include <stdio.h>
-      #define MAXLINE 1000     /* maximum input line size */
+      #define MAXLINE 1000
 
-      int getline(char line[], int maxline);
-      void copy(char to[], char from[]);
-
-      /* print longest input line */
-      main()
+      int main()
       {
-          int len;               /* current line length */
-          int max;               /* maximum length seen so far */
-          char 1ine[MAXLINE];    /* current input line */
-          char longest[MAXLINE]; /* longest line saved here */
+          int len;
+          int max = 0;
+          char lin[MAXLINE];
+          char longest[MAXLINE];
 
-          max = 0;
-          while ((len = getline(line, MAXLINE)) > 0) {
+          while ((len = getline(lin, MAXLINE)) > 0) {
 
-              if (len >max( {
-                  max = len;
-                  copy(longest, line);
-              }
-              printf("Length: %d\n", len);
-              printf("Text: %s", line);
+                printf("Longest: %d\n", len);
+                if (len > max) {
+                    max = len;
+                    copy(longest, lin);
+                }
+                printf("Text: %s", lin);
           }
-          if (max > 0)    /* there was a line */
+          if (max > 0)
+          {
               printf("Longest line:\n");
               printf("Length: %d\n", max);
               printf("Text: %s", longest);
-      }
+          }
           return 0;
       }
-
-      /* getline: read a line into s, return length */
-      int getline (char s[], int lim)
+      int getline(char s[], int lim)
       {
           int c, i;
-
-          for (i=0; i<lim-1 && (c=getchar()) !=EOF && cl='\n'; ++i)
+          for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c!= '\n'; ++i)
+          {
               s[i] = c;
-          if (c == ´\n´) {
+          }
+
+          if (c == '\n')
+          {
               s[i] = c;
               ++i;
           }
-          s[i] = ´\0´;
+
+          s[i] = '\0';
           return i;
       }
 
-      /* copy: copy 'from' into 'to'; assume to is big enough */
-      void copy (char to[] , char f rom[])
-      {
-          int i = 0;
-
-          i = 0;
-          while ((to[i] = from[i]) != '\0')
-              ++i;
-      }
+          copy(char to[], char from[])
+           {
+               int i = 0;
+               while ((to[i], from[i]) != '\0') {
+                    ++i;
+               }
+           }
