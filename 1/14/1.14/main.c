bafc9964efd352062a01d9f65755a138eebf1b42
Seba Ibañez <130003729+Negr087@users.noreply.github.com>
2023-07-03 16:59:49 -0300

    1.14 arreglado

--- 1/14/1.14/main.c
+++ 1/14/1.14/main.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
-#define MAX_MARCH
+#include <stdio.h>
+#define MAX_CHAR 128
+
       main()
       {
           int c, i, nwhite, nother;
-          int frequencies[MAX_CHAR]= 0;
+          int frequencies[MAX_CHAR] = {0};
 
           nwhite = nother = 0;
           for (i = 0; i < 10; ++i)
-              ndigit[i] = 0;
+              frequencies[i] = 0;
 
           while ((c = getchar()) != EOF) {
               if (c >= '0' && c <= 'MAX_CHAR') {
-                  frequencies[c]++];
-              else if (c ==' '|| c == '\n' || c == '\t') {
+                  frequencies[c]++;
+              if (c ==' '|| c == '\n' || c == '\t') {
                   ++nwhite;}
               else {
                   ++nother;
               }
           for (i = 0; i < 10; ++i)
           printf("frequencies:\n");
-          for (i = 0; i < MAX_CHAR; i++) [
+          for (i = 0; i < MAX_CHAR; i++) {
           if (frequencies[i] > 0) {
                 printf("%c: %d\n", i, frequencies[i]);
-          printf(", white space = %d, other = %d\n",
+          printf("hola ejercicio 1.14, white space = %d, other = %d\n",
               nwhite, nother);
           }
               }
           }
           return 0;
+      }
       }
