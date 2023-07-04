51e4ca96de559b2cb9817f8acf41757dcf176a61
Seba Ibañez <130003729+Negr087@users.noreply.github.com>
2023-07-04 11:45:15 -0300

    1.19

--- 1/19/1.19/main.c
+++ 1/19/1.19/main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define MAXLINE 1000
 
-int main()
-{
-    printf("Hello world!\n");
+int getline(char line[], int maxline);
+void reverse(char line[]);
+
+int main() {
+    char line[MAXLINE];
+
+    while (getline(line, MAXLINE) > 0) {
+        reverse(line);
+        printf("%s\n", line);
+    }
+
     return 0;
+}
+
+int getline(char s[], int lim) {
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+
+    s[i] = '\0';
+    return i;
+}
+
+void reverse(char s[]) {
+    int i, j;
+    char temp;
+
+    for (i = 0, j = strlen(s) - 1; i < j; ++i, --j) {
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
 }
