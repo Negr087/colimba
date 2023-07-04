a61466cdbcada9d849952df4dfcb889281ea1341
Seba Ibañez <130003729+Negr087@users.noreply.github.com>
2023-07-04 00:52:00 -0300

    1.18

--- 1/18/1.18/main.c
+++ 1/18/1.18/main.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define MAXLINE 1000
+
+int getline(char line[], int maxline);
 
 int main()
 {
-    printf("Hello world!\n");
+    char line[MAXLINE];
+    int i, c;
+
+    while ((c =getchar()) != EOF) {
+        if (c == '\n') {
+            line[i] = c;
+            ++i;
+            line[i] = '\0';
+
+            if (i > 1) {
+                printf("%s", line);
+            }
+
+            i = 0;
+        } else if (c != ' ' && c != '\t') {
+            line[i] = c;
+            ++i;
+        }
+    }
+
     return 0;
+}
+
+int getline(char line[], int maxline) {
+    int c, i;
+
+    for (i = 0; i < maxline - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        if (c != ' ' && c != '\t') {
+            line[i] = c;
+        }
+    }
+
+    if (c == '\n') {
+        line[i] = c;
+        ++i;
+    }
+
+    line[i] = '\0';
+    return i;
 }
